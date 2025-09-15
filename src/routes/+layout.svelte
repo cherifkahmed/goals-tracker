5828d948f2b87b83383ff4d5d89d0ce12667e9f4
cherifkahmed <cherif.k.ahmed@gmail.com>
2025-09-15 07:14:28 +0000

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<nav>
+    <h1>Goals Tracker</h1>
+</nav>
+
+<style>
+
+nav {
+    overflow: hidden;
+    background-color: darkgoldenrod;
+}
+</style>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import goals from '$lib/goals.json';
+</script>
+
+<div class="grid-container">
+	<h1>Goals</h1>
+	<h1>working</h1>
+	<h1>updates</h1>
+	{#each goals as goal, i (goal.id)}
+		<div>
+			<li>{i + 1} {goal.name}</li>
+		</div>
+
+		<div>
+			<li>{goal.works}</li>
+		</div>
+
+		<div>
+			<li>{goal.updates}</li>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.grid-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+	li {
+	    list-style: none;
+	}
+</style>
